6f523514de7131a24bbb80a4c0c19cfb66b6883c
Artikodin <artium.wasse@gmail.com>
2018-06-06 23:29:27 +0200

    add shape and physic

--- src/index.js
+++ src/index.js
@@ -44,11 +44,14 @@ function loadComplete(bufferList) {
     arrayDomaine = new Uint8Array(analyseur.fftSize);
     // On remplit le tableau avec les frequences du son
 
-    
-    analyseur.getByteFrequencyData(arrayFreq); 
-    console.log(arrayFreq);
-    analyseur.getByteTimeDomainData(arrayDomaine); 
-    console.log(arrayDomaine);
+    function getArrayFreq() {
+        analyseur.getByteFrequencyData(arrayFreq);
+        return arrayFreq;
+    }
+    function getArrayDomaine() {
+        analyseur.getByteTimeDomainData(arrayDomaine);
+        return arrayDomaine;
+    };
 
 
     ////////////////////////////////////////////////////////////////////////////////////
@@ -59,6 +62,16 @@ function loadComplete(bufferList) {
 
     let sphereArray = [];
 
+    const addEntityToScene = (components) => {
+        const element = document.createElement('a-entity')
+        for (var key in components) {
+            element.setAttribute(key, components[key]);
+        }
+        sceneEl.appendChild(element)
+    }
+
+
+
     // Fonction qui ajoute les composant goutte à la scène
     const makeItRain = (i) => {
         sphereArray.push(document.createElement('a-entity'));
@@ -66,7 +79,7 @@ function loadComplete(bufferList) {
         sceneEl.appendChild(sphereArray[i]);
     }
 
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 0; i++) {
         makeItRain(i)
     }
 
@@ -83,6 +96,11 @@ function loadComplete(bufferList) {
         return randomValue;
     }
 
+    const randomValueBetweenPosNeg = (i, j) => {
+        let randomValue = Math.floor(Math.random() * j) + i;
+        randomValue *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        return randomValue;
+    }
 
     ////////////////////////////////////////////////////////////////////////////////////
     //                   drop.js													  //
@@ -101,6 +119,8 @@ function loadComplete(bufferList) {
         init: function () {
             var data = this.data;
             var el = this.el;
+            // console.log("1 : ",randomValueBetweenPosNeg(0, 15))
+            // console.log("2 : ",randomValueBetweenPosNeg(0, 15))
             // Création de la forme de la goutte
             this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
             // Ajout de la couleur à la goutte
@@ -110,7 +130,7 @@ function loadComplete(bufferList) {
             // Ajoute la goutte à l'entité
             el.setObject3D('mesh', this.mesh);
             // Positionne la goutte aléatoirement
-            el.object3D.position.set(randomValueBetween(-15, 15), randomValueBetween(20, 30), randomValueBetween(-15, 15));
+            el.object3D.position.set(randomValueBetweenPosNeg(-15, 15), randomValueBetween(20, 30), randomValueBetweenPosNeg(-15, 15));
         },
         // Méthode executée plusieurs fois par seconde
         // Elle anime la goutte qui tombe du ciel
@@ -120,12 +140,158 @@ function loadComplete(bufferList) {
             if (currentPosition.y < -.5) {
                 this.reset();
             }
-            // analyseur.getByteTimeDomainData(arrayDomaine)
-            // console.log(arrayDomaine);
         },
         // Méthode qui repositionne la goutte aléatoirement dans le ciel
         reset: function () {
             this.el.object3D.position.y = randomValueBetween(20, 30);
         }
     });
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //                   square.js													  //
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    AFRAME.registerComponent('square', {
+        schema: {
+            width: { type: 'number', default: 1 },
+            height: { type: 'number', default: 1 },
+            depth: { type: 'number', default: 1 },
+            color: { type: 'color', default: '#FF0000' },
+            posX: { type: 'number', default: 0 },
+            posY: { type: 'number', default: 3 },
+            posZ: { type: 'number', default: -2 },
+        },
+
+        init: function () {
+            var data = this.data;
+            var el = this.el;
+            this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.height);
+            this.material = new THREE.MeshStandardMaterial({ color: data.color });
+            this.mesh = new THREE.Mesh(this.geometry, this.material);
+            el.setObject3D('mesh', this.mesh);
+            el.object3D.position.set(data.posX, data.posY, data.posZ);
+        }
+    });
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //                   triangle.js													  //
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    AFRAME.registerComponent('triangle', {
+        schema: {
+            radius: { type: 'number', default: 1 },
+            height: { type: 'number', default: 1 },
+            radialSegments: { type: 'number', default: 3 },
+            color: { type: 'color', default: '#FFE500' },
+            posX: { type: 'number', default: 0 },
+            posY: { type: 'number', default: 3 },
+            posZ: { type: 'number', default: -2 },
+        },
+
+        init: function () {
+            var data = this.data;
+            var el = this.el;
+            this.geometry = new THREE.ConeGeometry(data.radius, data.height, data.radialSegments);
+            this.material = new THREE.MeshStandardMaterial({ color: data.color });
+            this.mesh = new THREE.Mesh(this.geometry, this.material);
+            el.setObject3D('mesh', this.mesh);
+            el.object3D.position.set(0, 3, -2);
+            el.object3D.position.set(data.posX, data.posY, data.posZ);
+        }
+    });
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //                   sphere.js													  //
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    AFRAME.registerComponent('sphere', {
+        schema: {
+            radius: { type: 'number', default: 1 },
+            widthSegments: { type: 'number', default: 32 },
+            heightSegments: { type: 'number', default: 32 },
+            color: { type: 'color', default: '#000080' },
+            posX: { type: 'number', default: 0 },
+            posY: { type: 'number', default: 3 },
+            posZ: { type: 'number', default: -2 },
+        },
+
+        init: function () {
+            var data = this.data;
+            var el = this.el;
+            this.geometry = new THREE.SphereGeometry(data.radius, data.widthSegments, data.heightSegments);
+            this.material = new THREE.MeshStandardMaterial({ color: data.color });
+            this.mesh = new THREE.Mesh(this.geometry, this.material);
+            el.setObject3D('mesh', this.mesh);
+            el.object3D.position.set(data.posX, data.posY, data.posZ);
+        }
+    });
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //                   cylinder.js													  //
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    AFRAME.registerComponent('cylinder', {
+        schema: {
+            radiusTop: { type: 'number', default: .5 },
+            radiusBottom: { type: 'number', default: .5 },
+            height: { type: 'number', default: 1 },
+            radialSegments: { type: 'number', default: 32 },
+            color: { type: 'color', default: '#008000' },
+            posX: { type: 'number', default: 0 },
+            posY: { type: 'number', default: 3 },
+            posZ: { type: 'number', default: -2 },
+        },
+
+        init: function () {
+            var data = this.data;
+            var el = this.el;
+            this.geometry = new THREE.CylinderBufferGeometry(data.radiusTop, data.radiusBottom, data.height, data.radialSegments);
+            this.material = new THREE.MeshStandardMaterial({ color: data.color });
+            this.mesh = new THREE.Mesh(this.geometry, this.material);
+            el.setObject3D('mesh', this.mesh);
+            el.object3D.position.set(data.posX, data.posY, data.posZ);
+        }
+    });
+
+    AFRAME.registerComponent('smooth-appear', {
+        schema: {
+            scale: { type: 'number', default: 0 }
+        },
+
+        init: function () {
+            this.el.object3D.scale.set(this.data.scale, this.data.scale, this.data.scale)
+        },
+        tick: function () {
+            if (this.el.object3D.scale.x < 1 || this.el.object3D.scale.y < 1 || this.el.object3D.scale.z < 1) {
+                this.el.object3D.scale.x += .1
+                this.el.object3D.scale.y += .1
+                this.el.object3D.scale.z += .1
+            }
+        },
+    });
+
+
+    for (let i = 0; i < 5; i++) {
+        addEntityToScene({
+            'square': `posX: ${randomValueBetweenPosNeg(0, 15)}; posZ: ${randomValueBetweenPosNeg(0, 15)};`,
+            'dynamic-body': 'shape: Box',
+        });
+        addEntityToScene({
+            'sphere' : `posX: ${randomValueBetweenPosNeg(0, 15)}; posZ: ${randomValueBetweenPosNeg(0, 15)};`,
+            'dynamic-body': 'shape: sphere; sphereRadius: 1',
+        });
+        addEntityToScene({
+            'triangle': `posX: ${randomValueBetweenPosNeg(0, 15)}; posZ: ${randomValueBetweenPosNeg(0, 15)};`,
+            'dynamic-body': 'shape: box;',
+        });
+        addEntityToScene({
+            'cylinder': `posX: ${randomValueBetweenPosNeg(0, 15)}; posZ: ${randomValueBetweenPosNeg(0, 15)};`,
+            'dynamic-body': 'shape: Cylinder;',
+        });
+    }
+
+
+
+
 }
